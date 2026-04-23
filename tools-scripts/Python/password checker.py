0cf8eea319ad2501ea21b962bd17f89913a71350
Ashogbon Ayomikun Abdulazeez <kvnghayzed5@gmail.com>
2026-04-24 00:39:43 +0100

    Add files via upload

---
+++ tools-scripts/Python/password checker.py
@@ -0,0 +1,7 @@
+password=""
+while password != "eponbo1212":
+    password=input("what is your password?")
+    if password=="eponbo1212":
+        print("Access approved")
+    else:
+        print("Access denied, please try again")
